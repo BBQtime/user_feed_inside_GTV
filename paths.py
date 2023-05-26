28c065df284c74d3287a7270884a13fd23cca9db
Jintao Ren <jintaodk@gmail.com>
2023-05-26 10:37:09 +0200

    before major changes

---
+++ paths.py
@@ -0,0 +1,5 @@
+import os
+imgs = '/data/jintao/nnUNet/nnUNet_raw_data_base/nnUNet_raw_data/Task901_AUH/imagesTs'
+ref = '/data/jintao/nnUNet/nnUNet_raw_data_base/nnUNet_raw_data/Task901_AUH/labelsTs'
+feed_pred = '/data/jintao/nnUNet/nnUNet_results/nnUNet/3d_fullres/Task101_auh/nnUNetTrainerV2__nnUNetPlansv2.1/fold_0/test'
+pred = '/data/jintao/nnUNet/nnUNet_results/nnUNet/3d_fullres/Task901_AUH/nnUNetTrainerV2__nnUNetPlansv2.1/fold_0/test'
--- simuate_user_feeds.py
+++ simuate_user_feeds.py
@@ -7,16 +7,32 @@ from skimage.segmentation import clear_border
 from skimage.feature import peak_local_max
 import random
 from multiprocessing import Pool
-
-def simulate_user_feeds(gtv_path):
-    out_put_path = gtv_path.replace('labelsOd', 'imagesOd')
-    out_put_path = out_put_path.replace('.nii.gz','_0004.nii.gz')
-    print(out_put_path)
+def small_volumes(gtv_path):
     img = sitk.ReadImage(gtv_path)
     arr = sitk.GetArrayFromImage(img)
 
     cleared = clear_border(arr)
-    print(cleared.shape)
+
+    label_image = label(cleared)
+    #coords = peak_local_max(arr)
+    #print(coords)
+    #coords = np.asarray(np.column_stack(np.ma.where(arr >0)))
+    #print(coords)
+    for region in regionprops(label_image):
+        if region.area<100:
+            print(gtv_path)
+
+
+def simulate_user_feeds(gtv_path):
+    iter_limit = 10000
+    out_put_path = gtv_path.replace('labelsOd', 'imagesOd')
+    out_put_path = out_put_path.replace('.nii.gz','_0004.nii.gz')
+    #print(out_put_path)
+    img = sitk.ReadImage(gtv_path)
+    arr = sitk.GetArrayFromImage(img)
+
+    cleared = clear_border(arr)
+    #print(cleared.shape)
     label_image = label(cleared)
     #coords = peak_local_max(arr)
     #print(coords)
@@ -29,10 +45,16 @@ def simulate_user_feeds(gtv_path):
         bbox = region.bbox
         point_not_found = True
         iterations = 0
-        if region.area>100: # only label volume larger than 1000 pixels
+        volume = region.area
+        if volume>100: # only label volume larger than 100 pixels
             
-            while point_not_found and iterations<100000:
-                radius = random.choice([2,3,4,5])           
+            while point_not_found and iterations<iter_limit:
+                radius = random.choice([2,3,4,5]) 
+                if iterations > (iter_limit/3) and iterations % 100==0:
+                    print(iterations, gtv_path)
+                    if volume < 500:
+                        radius = 2
+                
                 iterations+=1
                 x = random.choice(np.linspace(bbox[0], bbox[3], dtype=int))
                 y = random.choice(np.linspace(bbox[1], bbox[4], dtype=int))
@@ -53,7 +75,8 @@ def simulate_user_feeds(gtv_path):
                     point_not_found = False
             
             new_arr = new_arr + temp_arr
-                    
+        if iterations > iter_limit:
+            print(gtv_path)   
         #print(region.area)
         #print(region._label_image)
     new_img = sitk.GetImageFromArray(new_arr)
@@ -68,5 +91,6 @@ if __name__ == '__main__':
 
     
     # #compare(files[0])
-    with Pool(64) as p:
+    with Pool(80) as p:
         p.map(simulate_user_feeds, gtv_path)
+        #p.map(small_volumes, gtv_path)
